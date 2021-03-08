c429bc7e89f1d57951f49fe4446cd7a5f2fbee0a
Ryczko <konradryczko1@gmail.com>
2021-03-08 20:35:31 +0100

    headers to connect

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,10 +5,21 @@ import appRouter from './routes';
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
-let connection_uri = process.env.MONGODB_DEV_URI || 'mongodb://localhost/playground';
+const connection_uri = process.env.MONGODB_DEV_URI || 'mongodb://localhost/playground';
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept , Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    next();
+});
+
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.use(appRouter);
 
