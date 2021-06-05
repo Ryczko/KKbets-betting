54a8b3ca483344190843d6c72904a6f6bc9e0be4
Ryczko <konradryczko1@gmail.com>
2021-06-05 16:41:22 +0200

    Banner Fixes

--- client/src/components/Banner/Banner.css.ts
+++ client/src/components/Banner/Banner.css.ts
@@ -4,8 +4,9 @@ export const StyledBanner = styled.div`
     img {
         width: 100%;
         display: block;
-        max-height: 390px;
+        max-height: 250px;
         border-radius: 4px;
-        object-fit: cover;
+        object-fit: contain;
+        background-color: #02b9cb;
     }
 `;
