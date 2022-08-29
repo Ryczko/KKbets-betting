c11e316b78e804c9cdd42aa8c3d966df7db806c9
Ryczko <konradryczko1@gmail.com>
2022-08-29 19:44:03 +0200

    Add admin button

--- apps/kkbets/src/app/layouts/TopNavigation/TopNavigation.tsx
+++ apps/kkbets/src/app/layouts/TopNavigation/TopNavigation.tsx
@@ -6,13 +6,24 @@ import Logo from '../../components/Logo/Logo';
 import { StyledHeader } from './TopNavigation.css';
 import Avatar from '../../components/Avatar/Avatar';
 import Currency from '../../components/Currency/Currency';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 function TopNavigation(): JSX.Element {
   const { isLogged, userData } = useContext(AuthContext);
 
+  const navigate = useNavigate();
+
   return (
     <StyledHeader>
-      <Logo />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Logo />
+        {userData?.admin && (
+          <Button className="display-above-md" style={{ marginLeft: '20px' }} fill click={() => navigate('/admin')}>
+            Admin panel
+          </Button>
+        )}
+      </div>
       {isLogged ? (
         <div className="user-data">
           <Currency value={userData.points} />
